1cd7d302082a685f9f2db5e58ef6058e4407fa1b
Medya Doran <98618193+dmedya@users.noreply.github.com>
2024-07-07 14:02:16 +0300

    Add files via upload

---
+++ lib/register_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kayıt Ol'),
+      ),
+      body: Center(
+        child: Text('Kayıt Ol Ekranı'),
+      ),
+    );
+  }
+}
